1f192ee262d9c8f7660e4bc0c5ec1dfc080e94d6
Michael Pinkham <mpinkham3@gmail.com>
2019-09-02 21:20:55 -0400

    Add zipper'd lattice type to game

--- src/Game.hs
+++ src/Game.hs
@@ -1,19 +1,41 @@
+{-# LANGUAGE TypeOperators #-}
+
 module Game ()
 
 where
--- import Control.Zipper
+
+import Control.Monad.State ( State )
+import Control.Zipper ( (:>>), Top )
 import Data.Vector ( MVector )
 
+newtype GameState = State Game
+
+data Game = Game { stage :: Stage
+                 , players :: [Player]
+                 , lattice :: Top :>> Lattice :>> Tac
+                 }
+
+data Stage = Start | Continue | End deriving ( Show, Eq, Ord )
+
+-- | The board on which the game is played.
 newtype Lattice = MVector Tac
 
+-- | Populates the 'Lattice'. Either a player has reserved a slot in the
+-- 'Lattice' via a Move or it is blank.
 type Tac = Maybe Move
 
 data Move = Move { player :: Player
                  , strategy :: Strategy
                  }
 
+
 data Strategy = Solicit
 
+-- | A Tic Tac Toe player. Each player has a unique mark, which identifies their
+-- 'Move's on the 'Lattice'.
 data Player = Player { name :: String
                      , mark :: Char
-                     }
+                     } deriving ( Show )
+
+instance Eq Player where
+  p1 == p2 = mark p1 == mark p2
