5a9c4d00753bd85b16a7dada5b40c556d0753c52
Michael Pinkham <mpinkham3@gmail.com>
2019-09-21 14:31:28 -0400

    Start adding an options parser

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,7 @@ module Main where
 import Lib
 import Game (route, respond, startGame)
 
--- route :: GameState -> String -> GameState
--- respond :: GameState -> String
-
 main :: IO ()
 main = interact (unlines
               . (map respond . scanl route startGame)
               . lines)
-
--- main :: IO ()
--- main = interact (unlines . (map show . scanl myAdd 0) . lines)
---
--- myAdd :: Int -> String -> Int
--- myAdd acc s = acc + (read s :: Int)
